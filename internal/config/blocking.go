c3b0e8c2b3e3f77fff1e9c523cdb0559fb20e72d
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-04-07 03:49:09 +0000

    Initial code transition

--- internal/config/blocking.go
+++ internal/config/blocking.go
@@ -13,8 +13,7 @@ func getBlacklistSettings(reader *reader) (settings blacklist.BuilderSettings, e
 	if err != nil {
 		return settings, err
 	}
-	settings.BlockSurveillance, err = reader.env.OnOff("BLOCK_SURVEILLANCE", params.Default("off"),
-		params.RetroKeys([]string{"BLOCK_NSA"}, reader.onRetroActive))
+	settings.BlockSurveillance, err = reader.env.OnOff("BLOCK_SURVEILLANCE", params.Default("off"))
 	if err != nil {
 		return settings, err
 	}
@@ -30,23 +29,33 @@ func getBlacklistSettings(reader *reader) (settings blacklist.BuilderSettings, e
 	if err != nil {
 		return settings, err
 	}
-	settings.AddBlockedIPs, settings.AddBlockedIPNets, err = getBlockedIPs(reader)
+	settings.AddBlockedIPs, err = getBlockedIPs(reader)
 	if err != nil {
 		return settings, err
 	}
-	privateIPs, privateIPNets, err := getPrivateAddresses(reader)
+	settings.AddBlockedIPNets, err = getBlockedIPNets(reader)
 	if err != nil {
 		return settings, err
 	}
-	settings.AddBlockedIPs = append(settings.AddBlockedIPs, privateIPs...)
-	settings.AddBlockedIPNets = append(settings.AddBlockedIPNets, privateIPNets...)
+	rebindingProtection, err := reader.env.OnOff("REBINDING_PROTECTION", params.Default("on"))
+	if err != nil {
+		return settings, err
+	}
+	if rebindingProtection {
+		privateIPNets, err := getPrivateIPNets()
+		if err != nil {
+			return settings, err
+		}
+		settings.AddBlockedIPNets = append(settings.AddBlockedIPNets, privateIPNets...)
+	}
+
 	return settings, nil
 }
 
 // getAllowedHostnames obtains a list of hostnames to unblock from block lists
 // from the comma separated list for the environment variable UNBLOCK.
 func getAllowedHostnames(reader *reader) (hostnames []string, err error) {
-	hostnames, err = reader.env.CSV("UNBLOCK")
+	hostnames, err = reader.env.CSV("ALLOWED_HOSTNAMES")
 	if err != nil {
 		return nil, err
 	}
@@ -73,35 +82,66 @@ func getBlockedHostnames(reader *reader) (hostnames []string, err error) {
 	return hostnames, nil
 }
 
-// getBlockedIPs obtains a list of IP addresses and IP networks to block from
+// getBlockedIPs obtains a list of IP addresses to block from
 // the comma separated list for the environment variable BLOCK_IPS.
-func getBlockedIPs(reader *reader) (ips []net.IP, ipNets []*net.IPNet, err error) {
+func getBlockedIPs(reader *reader) (ips []net.IP, err error) {
 	values, err := reader.env.CSV("BLOCK_IPS")
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-	ips, ipNets, err = convertStringsToIPs(values)
-	if err != nil {
-		return nil, nil, fmt.Errorf("invalid blocked IP string: %s", err)
-	}
-	return ips, ipNets, nil
-}
 
-func convertStringsToIPs(values []string) (ips []net.IP, ipNets []*net.IPNet, err error) {
-	ips = make([]net.IP, 0, len(values))
-	ipNets = make([]*net.IPNet, 0, len(values))
+	ips = make([]net.IP, len(values))
 	for _, value := range values {
 		ip := net.ParseIP(value)
-		if ip != nil {
-			ips = append(ips, ip)
-			continue
+		if ip == nil {
+			return nil, fmt.Errorf("invalid blocked IP: %s", err)
 		}
-		_, IPNet, err := net.ParseCIDR(value)
-		if err == nil && IPNet != nil {
-			ipNets = append(ipNets, IPNet)
-			continue
-		}
-		return nil, nil, fmt.Errorf("%s", value)
+		ips = append(ips, ip)
 	}
-	return ips, ipNets, nil
+
+	return ips, nil
+}
+
+// getBlockedIPNets obtains a list of IP networks (CIDR notation) to block from
+// the comma separated list for the environment variable BLOCK_IPNETS.
+func getBlockedIPNets(reader *reader) (ipNets []*net.IPNet, err error) {
+	values, err := reader.env.CSV("BLOCK_IPNETS")
+	if err != nil {
+		return nil, err
+	}
+
+	ipNets = make([]*net.IPNet, len(values))
+	for _, value := range values {
+		_, ipNet, err := net.ParseCIDR(value)
+		if err != nil {
+			return nil, fmt.Errorf("invalid blocked IP network CIDR: %s", err)
+		}
+		ipNets = append(ipNets, ipNet)
+	}
+
+	return ipNets, nil
+}
+
+func getPrivateIPNets() (privateIPNets []*net.IPNet, err error) {
+	privateCIDRs := []string{
+		"127.0.0.1/8",
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+		"169.254.0.0/16",
+		"::1/128",
+		"fc00::/7",
+		// "fe80::/10", - TODO intepreted as 0.0.0.0/0
+		"::ffff:0:0/96",
+	}
+	privateIPNets = make([]*net.IPNet, len(privateCIDRs))
+	for i := range privateCIDRs {
+		_, ipNet, err := net.ParseCIDR(privateCIDRs[i])
+		if err != nil {
+			return nil, err
+		}
+		privateIPNets[i] = ipNet
+	}
+
+	return privateIPNets, nil
 }
